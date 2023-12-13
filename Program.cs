1a5b2eff5978d9de7add522a875a313c61451331
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-12-13 09:54:02 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Arrayejercicio2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+            char[] fvocales = { 'a', 'c', 'i', 'a', 'a' };
+            if (a) 
+            {
+                for (int i = 0; i < vocales.Length; i++)
+                {
+                    for (int j = 0; j < fvocales.Length; j++)
+                    {
+                        if (vocales != fvocales)
+                        {
+                            a = false;
+                            break;
+                        }
+                    }
+                } 
+            } 
+        }
+    }
+}
